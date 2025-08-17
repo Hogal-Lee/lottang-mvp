23ab6183c286d40e5e1a858bdcac3e9a759362d9
Hogal-Lee <hogal3840@gmail.com>
2025-08-17 16:52:02 +0900

    chore: env.js로 Kakao JS 키 자동 로드

---
+++ web/env.js
@@ -0,0 +1,5 @@
+// Kakao Maps JavaScript API Key
+// ⚠️ 주의: 반드시 Kakao Developers 콘솔에서 "플랫폼 > Web"에 
+// https://hogal-lee.github.io / http://localhost:5500 도메인을 등록해야 함!
+
+window.KAKAO_JS_KEY = "ca26f9ae7a3d87a3526307104203c1a0";
